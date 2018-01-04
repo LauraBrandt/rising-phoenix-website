c4090397b93f20feaabd6e137ebbc280d6faf55d
Brandt <laurambrandt@gmail.com>
2018-01-04 10:37:08 -0300

    recombined App.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { StyleRoot } from 'radium';
+import PageRouter from './pageRouter';
+import Footer from './components/Footer';
 
 ReactDOM.render((
-  <App />
+  <StyleRoot>
+    <Router>
+      <div>
+        <PageRouter />
+        <Footer />
+      </div>
+    </Router>
+  </StyleRoot>
 ), document.getElementById('root'));
 
 registerServiceWorker();
