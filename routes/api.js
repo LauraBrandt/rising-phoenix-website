0913036e02de94e99a6ae357301a9c5f669fa3cd
Brandt <laurambrandt@gmail.com>
2018-01-04 17:21:30 -0300

    get login endpoint working

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
 
-router.get('/', (req, res) => {
-  res.send("api route");
+// router.get('/', (req, res, next) => {
+//   console.log(req);
+//   res.send("api route");
+// });
+
+router.post('/login', (req, res, next) => {
+  if (req.body.username && req.body.password) {
+    User.authenticate(req.body.username, req.body.password, (err, user) => {
+      if (err || !user) {
+        const err = new Error('Wrong user name or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.status(200).json({
+          message: "login ok",
+        });
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
 });
 
 router.get('/logout', function(req, res, next) {
