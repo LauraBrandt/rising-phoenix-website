548b8773492ed3e7166fdb17660a8c5387588a02
Brandt <laurambrandt@gmail.com>
2018-01-08 14:01:58 -0300

    fix jwt issuer error

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ const authCheck = jwt({
       jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
   audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}`,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
 
