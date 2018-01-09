fe0bb23ae4671a679e9841c15df430e8184ecbb8
Brandt <laurambrandt@gmail.com>
2018-01-08 21:31:51 -0300

    minor style modification

--- src/styles/admin/optionsStyles.js
+++ src/styles/admin/optionsStyles.js
@@ -9,14 +9,18 @@ const optionsStyles = {
     cursor: 'pointer',
     border: '1px solid rgba(0,0,0,0.3)', 
     borderRadius: '5px',
-    padding: '0.2em 0.4em',
     boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
     backgroundColor: 'white',
     fontSize: '1.5em',
     color: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     Link: {
+      padding: '0.2em 0.4em',
       color: '#111',
-      textDecoration: 'none'
+      textDecoration: 'none',
+      display: 'block',
     }
   },
   menu: {
