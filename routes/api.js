4e9c7f6966b643899e77768b73590bb770f8c165
Brandt <laurambrandt@gmail.com>
2018-01-08 22:12:58 -0300

    added validation to links CMS input

--- routes/api.js
+++ routes/api.js
@@ -85,39 +85,59 @@ router.post('/individual-sponsors', authCheck, (req, res) => {
 
 router.post('/links', authCheck, (req, res) => {
   let linksSent = req.body;
-  Links
-    .findOne({})
-    .exec((err, currLinks) => {
-      if (err) {
-        console.log(err)
-        const newError = new Error('An error occured fetching the links.');
-        res.status(err.status || 404).json({message: newError.message});
-      } else if (!currLinks) {
-        console.log('No links exist - creating new')
-        const newLinks = new Links(linksSent);
-        newLinks.save(function (err, updatedLinks) {
-          if (err) {
-            console.log(err)
-            const newError = new Error('Links were not saved.');
-            res.status(err.status || 404).json({message: newError.message});
-          }
-          res.send({message: 'Links successfully saved.'});
-         });
-      } else {
-        console.log('Links updating...')
-        currLinks.facebook = linksSent.facebook;
-        currLinks.twitter = linksSent.twitter;
-        currLinks.donate = linksSent.donate;
-        currLinks.save(function (err, updatedLinks) {
-          if (err) {
-            console.log(err)
-            const newError = new Error('Links were not saved.');
-            res.status(err.status || 404).json({message: newError.message});
-          }
-          res.send({message: 'Links successfully saved.'});
-        });
-      }
-    });
+  let linksValid = true
+
+  for (var linkKey in linksSent) {
+    if (!linksSent.hasOwnProperty(linkKey)) {
+      continue;
+    }
+
+    let sanitizedLink = xssFilters.inDoubleQuotedAttr(linksSent[linkKey]);
+
+    if (!validator.isURL(sanitizedLink) && sanitizedLink !== '') {
+      linksValid = false;
+      const uppercaseLinkKey = linkKey.charAt(0).toUpperCase() + linkKey.slice(1);
+      const message = `${uppercaseLinkKey} URL is not valid. Links not saved, please try again.`;
+      res.json({message: message});
+      break;
+    }
+    linksSent[linkKey] = sanitizedLink;
+  }
+
+  if (linksValid) {
+    Links
+      .findOne({})
+      .exec((err, currLinks) => {
+        if (err) {
+          console.log(err)
+          const newError = new Error('An error occured fetching the links.');
+          res.status(err.status || 404).json({message: newError.message});
+        } else if (!currLinks) {
+          console.log('No links exist - creating new')
+          const newLinks = new Links(linksSent);
+          newLinks.save(function (err, updatedLinks) {
+            if (err) {
+              console.log(err)
+              const newError = new Error('Links were not saved.');
+              res.status(err.status || 404).json({message: newError.message});
+            }
+            res.send({message: 'Links saved successfully.'});
+          });
+        } else {
+          currLinks.facebook = linksSent.facebook;
+          currLinks.twitter = linksSent.twitter;
+          currLinks.donate = linksSent.donate;
+          currLinks.save(function (err, updatedLinks) {
+            if (err) {
+              console.log(err)
+              const newError = new Error('Links were not saved.');
+              res.status(err.status || 404).json({message: newError.message});
+            }
+            res.send({message: 'Links saved successfully.'});
+          });
+        }
+      });
+    }
 });
 
 module.exports = router;
