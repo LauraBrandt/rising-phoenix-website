60e49cc9743a1aace98deaf066f723e944233754
Brandt <laurambrandt@gmail.com>
2018-01-20 17:19:41 -0300

    changed names from 'handler' to 'controller'

--- routes/api.js
+++ routes/api.js
@@ -11,13 +11,13 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 // route controllers
-const committeeMembersHandler = require('./controllers/committee-members-controller.js');
-const calendarHandler = require('./controllers/calendar-controller.js');
-const corporateSponsorsHandler = require('./controllers/corporate-sponsors-controller.js');
-const individualSponsorsHandler = require('./controllers/individual-sponsors-controller.js');
-const donateInfoHandler = require('./controllers/donate-info-controller.js');
-const donateLevelsHandler = require('./controllers/donate-levels-controller.js');
-const linksHandler = require('./controllers/links-controller.js');
+const committeeMembersController = require('./controllers/committee-members-controller.js');
+const calendarController = require('./controllers/calendar-controller.js');
+const corporateSponsorsController = require('./controllers/corporate-sponsors-controller.js');
+const individualSponsorsController = require('./controllers/individual-sponsors-controller.js');
+const donateInfoController = require('./controllers/donate-info-controller.js');
+const donateLevelsController = require('./controllers/donate-levels-controller.js');
+const linksController = require('./controllers/links-controller.js');
 
 /// AUTHENTICATION
 const authCheck = jwt({
@@ -35,97 +35,97 @@ const authCheck = jwt({
 
 /// ROUTES - GET
 router.get('/committee-members', (req, res) => {
-  committeeMembersHandler.get(res);
+  committeeMembersController.get(res);
 });
 
 router.get('/calendar', (req, res) => {
-  calendarHandler.get(res);
+  calendarController.get(res);
 });
 
 router.get('/corporate-sponsors', (req, res) => {
-  corporateSponsorsHandler.get(res);
+  corporateSponsorsController.get(res);
 });
 
 router.get('/individual-sponsors', (req, res) => {
-  individualSponsorsHandler.get(res);
+  individualSponsorsController.get(res);
 });
 
 router.get('/donate-info', (req, res) => {
-  donateInfoHandler.get(res);
+  donateInfoController.get(res);
 });
 
 router.get('/donate-levels', (req, res) => {
-  donateLevelsHandler.get(res);
+  donateLevelsController.get(res);
 });
 
 router.get('/links', (req, res) => {
-  linksHandler.get(res);
+  linksController.get(res);
 });
 
 
 /// ROUTES - POST
 router.post('/committee-members', authCheck, (req, res, next) => {  
-  committeeMembersHandler.post(req, res, next);
+  committeeMembersController.post(req, res, next);
 });
 
 router.post('/calendar', authCheck, (req, res, next) => {  
-  calendarHandler.post(req, res, next);
+  calendarController.post(req, res, next);
 });
 
 router.post('/corporate-sponsors', authCheck, upload.single('logoFile'), (req, res, next) => {
-  corporateSponsorsHandler.post(req, res, next);
+  corporateSponsorsController.post(req, res, next);
 });
 
 router.post('/individual-sponsors', authCheck, (req, res, next) => {
-  individualSponsorsHandler.post(req, res, next);
+  individualSponsorsController.post(req, res, next);
 });
 
 router.post('/donate-info', authCheck, (req, res, next) => {
-  donateInfoHandler.post(req, res, next);
+  donateInfoController.post(req, res, next);
 });
 
 router.post('/donate-levels', authCheck, (req, res, next) => {
-  donateLevelsHandler.post(req, res, next);
+  donateLevelsController.post(req, res, next);
 });
 
 router.post('/links', authCheck, (req, res, next) => {
-  linksHandler.post(req, res, next);
+  linksController.post(req, res, next);
 });
 
 
 /// ROUTES - DELETE
 router.delete('/committee-members', authCheck, (req, res, next) => {
-  committeeMembersHandler.delete(req, res, next);
+  committeeMembersController.delete(req, res, next);
 });
 
 router.delete('/calendar', authCheck, (req, res, next) => {
-  calendarHandler.delete(req, res, next);
+  calendarController.delete(req, res, next);
 });
 
 router.delete('/corporate-sponsors', authCheck, (req, res, next) => {
-  corporateSponsorsHandler.delete(req, res, next);
+  corporateSponsorsController.delete(req, res, next);
 });
 
 router.delete('/donate-levels', authCheck, (req, res, next) => {
-  donateLevelsHandler.delete(req, res, next);
+  donateLevelsController.delete(req, res, next);
 });
 
 
 /// ROUTES - PUT
 router.put('/committee-members', authCheck, (req, res, next) => {
-  committeeMembersHandler.put(req, res, next);
+  committeeMembersController.put(req, res, next);
 });
 
 router.put('/calendar', authCheck, (req, res, next) => {
-  calendarHandler.put(req, res, next);
+  calendarController.put(req, res, next);
 });
 
 router.put('/corporate-sponsors', authCheck, (req, res, next) => {
-  corporateSponsorsHandler.put(req, res, next);
+  corporateSponsorsController.put(req, res, next);
 });
 
 router.put('/donate-levels', authCheck, (req, res, next) => {
-  donateLevelsHandler.put(req, res, next);
+  donateLevelsController.put(req, res, next);
 });
 
 
