4463a4b2d9fc3ed8ee484586c71ef7a43d61b5e9
Laura Brandt <laurambrandt@gmail.com>
2018-02-10 13:30:32 -0300

    trying to get routes working

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 // routes
 const api = require("./routes/api");
-const site = require("./routes/site");
+// const site = require("./routes/site");
 
 // set up database
 mongoose.Promise = global.Promise;
@@ -38,7 +38,9 @@ mongoose.connect(process.env.MONGODB_URL_MLAB).then(
     app.use(express.static(path.resolve(__dirname, "build")));
 
     app.use("/api", api);
-    app.use("*", site);
+    app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
     app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
       res.status(err.status || 500).json({
